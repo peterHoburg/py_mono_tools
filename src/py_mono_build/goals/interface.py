7ebdab6bde6435362f2d7d659ef9b08f349f4c1d
Peter <peterHoburg@users.noreply.github.com>
2022-09-02 11:22:05 -0400

    Finished docs. Finally.

--- src/py_mono_build/goals/interface.py
+++ src/py_mono_build/goals/interface.py
@@ -1,20 +1,32 @@
+"""Contains the interfaces that goals will implement."""
 import abc
 import typing as t
 
 
 class Linter(abc.ABC):
+    """The interface that all linters will implement."""
+
     name: str
     parallel_run: bool
 
     def __init__(self, args: t.Optional[t.List[str]] = None):
+        """Will initialize the linter.
+
+        Args are passed through to the linter.
+        """
         if args is None:
             args = []
         self._args = args
 
     @abc.abstractmethod
     def run(self):
+        """Will run the linter.
+
+        Depending on the linter this may have side effects
+        """
         raise NotImplementedError
 
     @abc.abstractmethod
     def check(self):
+        """Will run the linter in check mode.This should NEVER change any files."""
         raise NotImplementedError
