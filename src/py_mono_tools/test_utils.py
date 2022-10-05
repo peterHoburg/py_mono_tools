9c432358945ebbc05c6bebb4a6822707755fed94
Peter <peterHoburg@users.noreply.github.com>
2022-10-05 08:47:40 -0400

    More testing vagrant fun times.

---
+++ src/py_mono_tools/test_utils.py
@@ -0,0 +1,24 @@
+import subprocess  # nosec B404
+
+"""
+vagrant up; vagrant ssh -c "cd /vagrant; poetry run pmt lint --check"
+"""
+
+
+def vagrant(cwd: str = None):
+    commands = [
+        "vagrant",
+        "up",
+        "--provision;",
+        "vagrant",
+        "ssh",
+        "-c",
+        '"cd /vagrant; poetry run pmt lint --check"',
+    ]
+    with subprocess.Popen(  # nosec B603
+        commands,
+        cwd=cwd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ) as process:
+        stdout_data, stderr_data = process.communicate()
--- test/py_mono_tools/test_cli.py
+++ test/py_mono_tools/test_cli.py
@@ -1,2 +1,40 @@
+import py_mono_tools
+import pathlib
+import subprocess  # nosec B404
+
+
+def vagrant(cwd: str = None):
+    up_commands = [
+        "vagrant",
+        "up",
+        "--provision",
+    ]
+    run_commands = [
+        "vagrant",
+        "ssh",
+        "default",
+        "-c",
+        '"cd /vagrant; poetry run pmt lint --check"',
+    ]
+
+    with subprocess.Popen(  # nosec B603
+        run_commands,
+        cwd=cwd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ) as process:
+        stdout_data, stderr_data = process.communicate()
+    print(stdout_data)
+    return stderr_data + stdout_data
+
+
 def test_help():
-    pass
+    """Test help."""
+    path = pathlib.Path(__file__)
+    project_root = path
+    while project_root.name != "test":
+        project_root = project_root.parent
+    project_root = project_root.parent
+    cwd = project_root.joinpath("example_repos/docker_module")
+    result = vagrant(cwd=cwd.absolute())
+    assert result == 0
