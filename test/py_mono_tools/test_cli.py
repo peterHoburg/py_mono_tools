a7e208e1ad9d3a10e0ca649a26f8c23690fd9c39
Peter <peterHoburg@users.noreply.github.com>
2022-10-12 23:09:59 -0400

    Added more tests.

--- test/py_mono_tools/test_cli.py
+++ test/py_mono_tools/test_cli.py
@@ -10,6 +10,18 @@ def test_help():
     assert b"Usage: pmt" in result
 
 
+def test_isort():
+    cwd = example_repo_path("docker_module")
+    returncode, result = vagrant(commands="poetry run pmt lint --check -s isort", cwd=cwd.absolute())
+    assert b"main.py Imports are incorrectly sorted and/or formatted" in result
+
+
+def test_mypy():
+    cwd = example_repo_path("docker_module")
+    returncode, result = vagrant(commands="poetry run pmt lint --check -s mypy", cwd=cwd.absolute())
+    assert b"main.py:6: \x1b[1m\x1b[31merror:\x1b(B\x1b[m Missing return statement" in result
+
+
 def test_py_doc_string_formatter():
     cwd = example_repo_path("docker_module")
     returncode, result = vagrant(commands="poetry run pmt lint --check -s py_doc_string_formatter", cwd=cwd.absolute())
