ad80989d257eeb865b051b0389364d3fa5a805f9
Peter <peterHoburg@users.noreply.github.com>
2022-10-13 12:16:15 -0400

    Refactored tests.

--- src/py_mono_tools/test_utils.py
+++ src/py_mono_tools/test_utils.py
@@ -1,9 +1,8 @@
-import subprocess  # nosec B404
 import os
-import pty
-import typing as t
 import pathlib
-import pytest
+import pty
+import subprocess  # nosec B404
+import typing as t
 
 
 def run_command_in_tty(command: str, cwd: pathlib.Path) -> t.Tuple[int, bytes]:
@@ -35,26 +34,10 @@ def run_command_in_tty(command: str, cwd: pathlib.Path) -> t.Tuple[int, bytes]:
     return returncode, output
 
 
-@pytest.fixture()
-def vagrant(commands: str, cwd: pathlib.Path = None) -> t.Generator[t.Tuple[int, bytes]]:
-    status_commands = "vagrant status"
-    up_commands = "vagrant up --provision"
-    run_commands = f'vagrant ssh default --command "cd /vagrant; {commands}"'
-    halt_commands = "vagrant halt"
-
-    returncode, output = run_command_in_tty(status_commands, cwd=cwd)
-
-    if b"poweroff" in output:
-        returncode, output = run_command_in_tty(up_commands, cwd=cwd)
-        if returncode != 0:
-            raise RuntimeError("vagrant up failed with output: %s", output)
-
+def vagrant_ssh(command: str, cwd: pathlib.Path) -> t.Tuple[int, bytes]:
+    run_commands = f'vagrant ssh default --command "cd /vagrant; {command}"'
     returncode, output = run_command_in_tty(run_commands, cwd=cwd)
-    try:
-        yield returncode, output
-    except:
-        run_command_in_tty(halt_commands, cwd=cwd)
-        raise
+    return returncode, output
 
 
 def example_repo_path(repo: str) -> pathlib.Path:
--- test/py_mono_tools/main/test_cli.py
+++ test/py_mono_tools/main/test_cli.py
@@ -1,34 +1,39 @@
-from py_mono_tools.test_utils import vagrant, example_repo_path
+import pathlib
 
-# TODO halt vagrant post tests, start pre tests.
+import pytest
 
-
-def test_help():
-    """Test the help command"""
-    cwd = example_repo_path("docker_module")
-    returncode, result = vagrant(commands="poetry run pmt --help", cwd=cwd.absolute())
-    assert b"Usage: pmt" in result
+from py_mono_tools.test_utils import vagrant_ssh
+import typing as t
 
 
-def test_isort():
-    cwd = example_repo_path("docker_module")
-    returncode, result = vagrant(commands="poetry run pmt lint --check -s isort", cwd=cwd.absolute())
-    assert b"main.py Imports are incorrectly sorted and/or formatted" in result
+@pytest.mark.parametrize("vagrant", ["docker_module"], indirect=["vagrant"])
+@pytest.mark.parametrize("conf_name", ["", "-n example_docker_module"])
+class TestCli:
+    def test_help(self, vagrant: pathlib.Path, conf_name: t.Optional[str]) -> None:
+        cwd = vagrant
+        returncode, result = vagrant_ssh(command=f"poetry run pmt {conf_name} --help", cwd=cwd.absolute())
+        assert b"Usage: pmt" in result
 
+    def test_isort(self, vagrant: pathlib.Path, conf_name: t.Optional[str]) -> None:
+        cwd = vagrant
+        returncode, result = vagrant_ssh(
+            command=f"poetry run pmt {conf_name} lint --check -s isort",
+            cwd=cwd.absolute(),
+        )
+        assert b"main.py Imports are incorrectly sorted and/or formatted" in result
 
-def test_mypy():
-    cwd = example_repo_path("docker_module")
-    returncode, result = vagrant(commands="poetry run pmt lint -s mypy", cwd=cwd.absolute())
-    assert b"main.py:6: \x1b[1m\x1b[31merror:\x1b(B\x1b[m Missing return statement" in result
+    def test_mypy(self, vagrant: pathlib.Path, conf_name: t.Optional[str]) -> None:
+        cwd = vagrant
+        returncode, result = vagrant_ssh(
+            command=f"poetry run pmt {conf_name} lint -s mypy",
+            cwd=cwd.absolute(),
+        )
+        assert b"main.py:6: \x1b[1m\x1b[31merror:\x1b(B\x1b[m Missing return statement" in result
 
-
-def test_mypy_specify_conf_name():
-    cwd = example_repo_path("docker_module")
-    returncode, result = vagrant(commands="poetry run pmt -n example_docker_module lint -s mypy", cwd=cwd.absolute())
-    assert b"main.py:6: \x1b[1m\x1b[31merror:\x1b(B\x1b[m Missing return statement" in result
-
-
-def test_py_doc_string_formatter():
-    cwd = example_repo_path("docker_module")
-    returncode, result = vagrant(commands="poetry run pmt lint --check -s py_doc_string_formatter", cwd=cwd.absolute())
-    assert b"Lint result: py_doc_string_formatter 0" in result
+    def test_py_doc_string_formatter(self, vagrant: pathlib.Path, conf_name: t.Optional[str]) -> None:
+        cwd = vagrant
+        returncode, result = vagrant_ssh(
+            command=f"poetry run pmt {conf_name} lint --check -s py_doc_string_formatter",
+            cwd=cwd.absolute(),
+        )
+        assert b"Lint result: py_doc_string_formatter 0" in result
